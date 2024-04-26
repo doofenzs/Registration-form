22c542bb187dff61d455d65e78960dbdfb954959
doofenzs <salunkheashu99@gmail.com>
2024-04-26 14:45:20 +0530

    Your commit message here

---
+++ src/Components/Form.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+const Form = () => {
+    const data = {name:"", email:"", password:""};
+    const [inputData, setInputData] = useState(data);
+    const [flag, setFlag] = useState(false);
+
+    useEffect(() => {
+        console.log("Registered");
+    }, [flag]);
+
+    function handleData(e) {
+        const { name, value } = e.target;
+        setInputData({...inputData, [name]: value});
+        console.log(inputData);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!inputData.name || !inputData.email || !inputData.password) {
+            alert("All fields are mandatory");
+        } else {
+            setFlag(true);
+        }
+    }
+
+    return (
+        <>
+            <pre>{flag && <h2 className='ui-define'>Hello {inputData.name},
+            You've registered successfully</h2>}</pre>
+
+            <form className='container' onSubmit={handleSubmit}>
+                <div className='header'>
+                    <h1>Registration Form</h1>
+                </div>
+
+                <div>
+                    <input type="text" placeholder='Enter your name' name='name'
+                        value={inputData.name} onChange={handleData} />
+                </div>
+
+                <div>
+                    <input type="text" placeholder='Enter your email' name='email'
+                        value={inputData.email} onChange={handleData} />
+                </div>
+
+                <div>
+                    <input type="text" placeholder='Enter your password' name='password'
+                        value={inputData.password} onChange={handleData} />
+                </div>
+
+                <div>
+                    <button type='submit'>Submit</button>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default Form;
